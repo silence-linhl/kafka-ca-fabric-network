3277fb5e39a0af9b8727c903ae52f4e3e61840b5
banbanpeppa <1262098118@qq.com>
2019-03-11 16:18:16 +0800

    Up new - add script to start cluster

---
+++ start-cluster-org2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Copyright IBM Corp All Rights Reserved
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Exit on first error, print all commands.
+set -ev
+
+# don't rewrite paths for Windows Git Bash users
+export MSYS_NO_PATHCONV=1
+
+docker-compose -f ./cluster-config/docker-compose-org2.yml down
+docker-compose -f ./cluster-config/docker-compose-cli-org2.yml down
+
+
+docker-compose -f ./cluster-config/docker-compose-org2.yml up -d
+docker-compose -f ./cluster-config/docker-compose-cli-org2.yml up -d
